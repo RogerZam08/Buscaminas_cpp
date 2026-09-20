026e1e481824313d963115f47fa3880a95c4f263
Roger Zambrano <roger.zambrano.faubla@gmail.com>
2026-09-19 19:34:48 -0500

    Inicialización del motor con CMake y C++17

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // Almacenamos este string literal en la sección de solo lectura del binario
+    std::cout << "¡Motor del Buscaminas iniciado exitosamente!" << std::endl;
+    return 0;
+}
